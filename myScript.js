c15d4dd34956a5eb560a666c2b1763b7bcd83e04
amantirpude <amantirpude79@gmail.com>
2023-11-03 19:19:29 +0530

    made external javascript file

---
+++ myScript.js
@@ -0,0 +1,13 @@
+var tabLinks = document.getElementsByClassName("tab-links");
+var tabContents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname,event){
+    for(let tablink of tabLinks){
+        tablink.classList.remove("active-link");
+    }
+    for(let tabcontent of tabContents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab")
+}
